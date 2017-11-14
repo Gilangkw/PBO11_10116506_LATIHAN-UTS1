6384f0046dab6289d5579fe40252cf5aa034e3db
Gilangkw <32380279+Gilangkw@users.noreply.github.com>
2017-11-14 16:02:23 +0700

    Add files via upload

---
+++ src/com/gilang/model/Modelmahasiswa.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gilang.model;
+
+/**
+ *
+ * @author gilang kusuma wijaya
+ */
+public class Modelmahasiswa {
+    private String nim;
+    private String nama;
+    
+    public Modelmahasiswa(String nim, String nama){
+        this.nim = nim;
+        this.nama = nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    
+}
